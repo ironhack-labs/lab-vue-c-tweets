c54681dc80c34f606299bf24a7acbc60f301d33e
Pablo <pablobeneyto1@gmail.com>
2023-04-13 21:05:07 +0200

    First done

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,51 @@
 <template>
   <div class="app">
-    <Tweet />
+    <Tweet 
+      v-for="(tweet,index) in tweets"
+      :key="index"
+      :user="tweet.user"
+      :timestamp="tweet.timestamp"
+      :message="tweet.message"
+    />
   </div>
 </template>
 
-<script setup>
-  import { ref } from 'vue';
-  import Tweet from './components/Tweet.vue';
-
-  const tweets = ref([
-    {
-      user: {
-        name: "Thoughts of Dog®",
-        image: "https://i.imgur.com/b0EdHVV.jpg",
-        handle: "dog_feelings",
-      },
-      timestamp: "1h ago",
-      message: "the human likes to say. that i live here rent free. but i would argue. this housing accommodation. is my payment. for a lifetime of love. and excellent company",
-    },
-    {
-      user: {
-        name: "Thoughts of Dog®",
-        image: "https://i.imgur.com/b0EdHVV.jpg",
-        handle: "dog_feelings",
-      },
-      timestamp: "2h ago",
-      message: "sometimes. the human presses their noggin against mine. to figure out what i’m thinking. so i just think really hard. about how much i love them. and hope they figure it out",
-    },
-    {
-      user: {
-        name: "Thoughts of Dog®",
-        image: "https://i.imgur.com/b0EdHVV.jpg",
-        handle: "dog_feelings",
-      },
-      timestamp: "3h ago",
-      message: "here is what. i plan to accomplish today: \n\n2. bark loudly. but at nothing \n7. lose my ball under the couch\n7b. politely ask the human. to get my ball\n3. immediately lose it again. under the same couch\n4. big nap. you have worked hard\n2. repeat",
-    }
-]);
+<script>
+import Tweet from './components/Tweet.vue';
+export default {
+    data: () => ({
+        tweets: [
+            {
+                user: {
+                    name: "Thoughts of Dog®",
+                    image: "https://i.imgur.com/b0EdHVV.jpg",
+                    handle: "dog_feelings",
+                },
+                timestamp: "1h ago",
+                message: "the human likes to say. that i live here rent free. but i would argue. this housing accommodation. is my payment. for a lifetime of love. and excellent company",
+            },
+            {
+                user: {
+                    name: "Thoughts of Dog®",
+                    image: "https://i.imgur.com/b0EdHVV.jpg",
+                    handle: "dog_feelings",
+                },
+                timestamp: "2h ago",
+                message: "sometimes. the human presses their noggin against mine. to figure out what i’m thinking. so i just think really hard. about how much i love them. and hope they figure it out",
+            },
+            {
+                user: {
+                    name: "Thoughts of Dog®",
+                    image: "https://i.imgur.com/b0EdHVV.jpg",
+                    handle: "dog_feelings",
+                },
+                timestamp: "3h ago",
+                message: "here is what. i plan to accomplish today: \n\n2. bark loudly. but at nothing \n7. lose my ball under the couch\n7b. politely ask the human. to get my ball\n3. immediately lose it again. under the same couch\n4. big nap. you have worked hard\n2. repeat",
+            },
+        ]
+    }),
+    components: { Tweet }
+}
 </script>
 
 <style>
@@ -48,22 +57,18 @@
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
-
   .app {
     text-align: center;
   }
-
   .app-logo {
     height: 40vmin;
     pointer-events: none;
   }
-
   @media (prefers-reduced-motion: no-preference) {
     .app-logo {
       animation: App-logo-spin infinite 20s linear;
     }
   }
-
   .app-header {
     background-color: #282c34;
     min-height: 100vh;
@@ -74,11 +79,9 @@
     font-size: calc(10px + 2vmin);
     color: white;
   }
-
   .app-link {
     color: #61dafb;
   }
-
   @keyframes App-logo-spin {
     from {
       transform: rotate(0deg);
@@ -87,11 +90,9 @@
       transform: rotate(360deg);
     }
   }
-
   .app {
     padding: 20px;
   }
-
   .tweet {
     margin: 0 auto;
     min-width: 480px;
@@ -103,53 +104,44 @@
     font-size: 1.1rem;
     line-height: 18px;
   }
-
   .body {
     display: flex;
     flex-direction: column;
     width: 100%;
   }
-
   .top {
     display: flex;
     line-height: 1.4rem;
   }
-
   .more {
     margin-left: 20px;
   }
-
   .profile {
     width: 50px;
     height: 50px;
     border-radius: 50px;
     margin-right: 10px;
   }
-
   .name {
     font-weight: bold;
     margin-bottom: 0.5em;
     margin-right: 0.3em;
   }
-
   .handle {
     color: #536471;
     margin-left: 3px;
     margin-right: 3px;
   }
-
   .timestamp {
     color: #536471;
     font-size: 1.1rem;
   }
-
   .timestamp::before {
     content: "·";
     padding-right: 0.3em;
     font-size: 1.1rem;
     font-weight: 600;
   }
-
   .message {
     text-align: left;
     font-stretch: 100%;
@@ -158,7 +150,6 @@
     overflow-wrap: break-word;
     white-space: pre-wrap;
   }
-
   .actions {
     padding: 5px 5px;
     width: 360px;
@@ -166,5 +157,4 @@
     justify-content: space-between;
     color: #536471;
   }
-
 </style>
--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,40 +1,50 @@
 <template>
-  <div class="tweet">
-    <img
-      src="https://i.imgur.com/9yw1Fyw.jpg"
-      class="profile"
-      alt="profile"
-    />
+  <div className="tweet">
 
-    <div class="body">
-      <div class="top">
-        <span class="user">
-          <span class="name">Ironhack</span>
-          <span class="handle">@ironhack</span>
-        </span>
+    <ProfileImage :image="user.image" />
 
-        <span class="timestamp">Nov 30, 2020</span>
+    <div className="body">
+      <div className="top">
+
+        <User :user="user" />
+        <Timestamp :timestamp="timestamp" />
+
       </div>
 
-      <p class="message">
-        On December 7th, we will be hosting a #webinar that will introduce you
-        to #SQL! Are you ready? 🚀
-      </p>
+      <Message :message="message" />
 
-      <div class="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-      </div>
+      <Actions />
     </div>
 
     <i class="fas fa-ellipsis-h"></i>
   </div>
 </template>
 
-<style scoped>
+<script>
+import User from './User.vue';
+import Timestamp from './Timestamp.vue';
+import ProfileImage from './ProfileImage.vue';
+import Message from './Message.vue';
+import Actions from './Actions.vue';
+export default {
+  name: "Tweet",
+  components: {
+    User,
+    Timestamp,
+    ProfileImage,
+    Message,
+    Actions
+},
+  props: {
+    image: String,
+    user: Object,
+    timestamp: String,
+    message: String,
+  },
+}
+</script>
+
+<style>
 a {
   color: #42b983;
 }

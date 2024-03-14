8c9d8d8cab224290cf99861fce080fabb45ef317
MarcMarimon <crammarimon@hotmail.com>
2024-03-14 20:48:23 +0100

    Lab done

---
+++ src/components/Timestamp.vue
@@ -0,0 +1,8 @@
+<script setup>
+defineProps({
+  timestamp: String,
+});
+</script>
+<template>
+  <span class="timestamp">{{ timestamp }}</span>
+</template>
---
+++ src/components/User.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  name: String,
+  handle: String,
+});
+</script>
+<template>
+  <span class="user">
+    <span class="name">{{ name }}</span>
+    <span class="handle">{{ "@" + handle }}</span>
+  </span>
+</template>

29be0e26d0ad51434a369f34b05d53d2567ecc75
Karen Schneider <karen-schneider@outlook.com>
2024-03-14 19:06:41 +0100

    primera iteracion

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,25 +1,28 @@
+<script setup>
+const props = defineProps({
+  tweet: {
+    type: Object,
+    required: true,
+  },
+});
+/* console.log(props.tweet) */
+</script>
+
 <template>
   <div class="tweet">
-    <img
-      src="https://i.imgur.com/9yw1Fyw.jpg"
-      class="profile"
-      alt="profile"
-    />
+    <img :src="props.tweet.user.image" class="profile"/>
 
     <div class="body">
       <div class="top">
         <span class="user">
-          <span class="name">Ironhack</span>
-          <span class="handle">@ironhack</span>
+          <span class="name">{{ props.tweet.user.name }}</span>
+          <span class="handle">{{ props.tweet.user.handle }}</span>
         </span>
 
-        <span class="timestamp">Nov 30, 2020</span>
+        <span class="timestamp">{{ props.tweet.timestamp }}</span>
       </div>
 
-      <p class="message">
-        On December 7th, we will be hosting a #webinar that will introduce you
-        to #SQL! Are you ready? 🚀
-      </p>
+      <p class="message">{{ props.tweet.message }}</p>
 
       <div class="actions">
         <!-- Font Awesome icons -->

ff808b93bb8a0ec14f04f67c033d1f584e526f01
Joan Vila Valls <joan.o.jan@gmail.com>
2024-03-14 18:52:06 +0100

    iteration 2

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,24 +1,26 @@
+<script setup>
+const props = defineProps({
+  tweet: Object
+})
+console.log(props.tweet.user.name)
+</script>
+
 <template>
   <div class="tweet">
-    <img
-      src="https://i.imgur.com/9yw1Fyw.jpg"
-      class="profile"
-      alt="profile"
-    />
+    <img :src="props.tweet.user.image" class="profile" alt="profile" />
 
     <div class="body">
       <div class="top">
         <span class="user">
-          <span class="name">Ironhack</span>
-          <span class="handle">@ironhack</span>
+          <span class="name">{{ props.tweet.user.name }}</span>
+          <span class="handle">{{ props.tweet.user.handle }}</span>
         </span>
 
-        <span class="timestamp">Nov 30, 2020</span>
+        <span class="timestamp">{{ props.tweet.timestamp }}</span>
       </div>
 
       <p class="message">
-        On December 7th, we will be hosting a #webinar that will introduce you
-        to #SQL! Are you ready? 🚀
+        {{ props.tweet.message }}
       </p>
 
       <div class="actions">

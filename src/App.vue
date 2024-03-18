cab1c602fd8ad3e78f4536ccb80dda42ca0efa64
Andybetan <andres_betan@hotmail.com>
2024-03-18 20:02:32 +0100

    commit components actualizado

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <Tweet />
+    <Tweet v-for="(tweet, index) in tweets" :key="index" :tweetData="tweet" />
   </div>
 </template>
 
---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,11 @@
+<template>
+    <img :src="image" class="profile" alt="profile" />
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      image: String,
+    },
+  };
+  </script>
